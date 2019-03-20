6698b739d58e58d2d1ab4ce2a72c82791b00fde5
Brian Chung <brian.chung@zuhlke.com>
2019-03-20 09:50:54 +0800

    add additional test to test roll call with more than 2 parameters

--- tests/rollRotuerTest.js
+++ tests/rollRotuerTest.js
@@ -32,6 +32,20 @@ describe('Send a POST to /api/v1/roll', () => {
     });
   });
 
+  describe('With more than 2 parameters', () => {
+    it('Should generate a random number using the first 2 parameters as lower bound and upper bound', (done) => {
+      apiRequest
+        .post(ROLL_END_POINT)
+        .send({ text: '0 100 1000' })
+        .set('Accept', 'application/json')
+        .expect(200)
+        .end((err) => {
+          if (err) return done(err);
+          return done();
+        });
+    });
+  });
+
   describe('Without parameters', () => {
     it('Should generate a random number within 0 and 100', (done) => {
       apiRequest
