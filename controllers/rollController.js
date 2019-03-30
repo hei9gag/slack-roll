72c5a1d8acc38dc9b36d5ffb03b95b38daab867c
Brian <brianhei@gmail.com>
2019-03-30 22:48:37 +0800

    add post example

--- controllers/rollController.js
+++ controllers/rollController.js
@@ -1,21 +1,9 @@
+import axios from 'axios';
 import RandomNumberGenerator from '../utils/randomGenerator';
 
 class RollController {
     rollARandomNumber = (req, res) => {
-      // example input
-      /*
-      req:{
-        "token":"saV9Tv8iP8bBiLqdbiXyXCrG",
-        "team_id":"TH19Z6JEA",
-        "team_domain":"zuhlkehk",
-        "channel_id":"CGXEHAK8Q",
-        "channel_name":"general",
-        "user_id":"UH06UQDTP",
-        "user_name":"brian.chung",
-        "command":"/roll","text":"",
-        "response_url":"https://hooks.slack.com/commands/TH19Z6JEA/595083220103/imIxVOjIlPrHvLKia9ahISWo","trigger_id":"593979297364.579339222486.1fa6c03a1f94d25cc63aa853b468032a"}
-      */
-      const { text } = req.body;
+      const { response_url, text } = req.body;
       let lowerBound = 0;
       let upperBound = 100;
 
@@ -42,6 +30,8 @@ class RollController {
         }
       }
       const result = RandomNumberGenerator.generate(lowerBound, upperBound);
+      console.log('response url:' + response_url);
+      this.sendRequest(response_url);
       return res.status(200).send(
         {
           response_type: 'in_channel',
@@ -51,6 +41,17 @@ class RollController {
     }
 
     isValidInputFormat = splitedString => splitedString.length <= 2
+
+    sendRequest = (url) => {
+      axios.post(url, {
+        response_type: 'in_channel',
+        text: 'PK1234'
+      }).then((response) => {
+
+      }).catch((error) => {
+
+      });
+    }
 }
 
 const rollController = new RollController();
