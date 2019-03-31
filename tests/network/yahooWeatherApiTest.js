f7d4797866c3fffc592e3953e5fdc63070488cdd
Brian <brianhei@gmail.com>
2019-03-31 20:57:14 +0800

    add test case for weather api and client

--- tests/network/yahooWeatherApiTest.js
+++ tests/network/yahooWeatherApiTest.js
@@ -1,9 +1,9 @@
 import { expect } from 'chai';
-import YahooWeatherApi from '../../network/weather/yahooWeatherApi';
+import yahooWeatherApi from '../../network/weather/yahooWeatherApi';
 
 describe('Send request to fetch weather info', () => {
   it('should return weather json', (done) => {
-    const result = YahooWeatherApi.fetchWeatherByLocation('hongkong');
+    const result = yahooWeatherApi.fetchWeatherByLocation('hongkong');
     result.then((data) => {
       expect(data).to.be.not.a('null');
       done();
