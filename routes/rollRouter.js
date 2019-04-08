20819babc0515dd46f17d8ff4311f91e8d6614e3
Brian <brianhei@gmail.com>
2019-04-08 22:56:54 +0800

    remove get

--- routes/rollRouter.js
+++ routes/rollRouter.js
@@ -5,7 +5,6 @@ const router = express.Router();
 
 export const ROLL_END_POINT = '/api/v1/roll';
 
-router.get('/', RollController.rollARandomNumber);
 router.post('/', RollController.rollARandomNumber);
 
 export default router;
--- routes/weatherRouter.js
+++ routes/weatherRouter.js
@@ -4,7 +4,6 @@ import weatherController from '../controllers/weatherController';
 const router = express.Router();
 export const WEATHER_END_POINT = '/api/v1/weather';
 
-router.get('/', weatherController.fetchWeather);
 router.post('/', weatherController.fetchWeather);
 
 export default router;
