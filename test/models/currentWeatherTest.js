a1779f04ac52d0b364a7ff52b9df776883fc67a2
Brian Chung <brian.chung@zuhlke.com>
2019-04-10 15:16:29 +0800

    added test case to create weather detail object from json

--- test/models/currentWeatherTest.js
+++ test/models/currentWeatherTest.js
@@ -1,5 +1,8 @@
+import fs from 'fs';
 import { expect } from 'chai';
 import CurrentWeather from '../../models/currentWeather';
+import parse from '../../parsers/weatherJsonParser';
+import WeatherDetail from '../../models/weatherDetail';
 
 describe('Create CurrentWeather model', () => {
   it('should return a new CurrentWeather', () => {
@@ -22,5 +25,19 @@ describe('Get weather emoji', () => {
   it('should return the description emoji', () => {
     const model = new CurrentWeather('Mostly Clear', 33, 76);
     expect(model.getWeatherEmoji()).to.be.equals(':sunny:');
+  });
+});
+
+describe('Receive a weather detail json', () => {
+  it('Should returns a WeatherDetail object', () => {
+    const jsonData = fs.readFileSync('test/resources/weather.json');
+    const weatherDetailData = JSON.parse(jsonData);
+    const weatherDetail = WeatherDetail.fromJson(weatherDetailData);
+    const { location } = weatherDetailData;
+    expect(weatherDetail.location.city).to.be.equals(location.city);
+    expect(weatherDetail.location.region).to.be.equals(location.region);
+    expect(weatherDetail.location.country).to.be.equals(location.country);
+    expect(weatherDetail.location.lat).to.be.equals(location.lat);
+    expect(weatherDetail.location.long).to.be.equals(location.long);
   });
 });
