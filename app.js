fdb3594aa1a300b8b12539527becdc2fea21ad11
Brian Chung <brian.chung@zuhlke.com>
2019-04-11 15:53:00 +0800

    fix eslint warning

--- app.js
+++ app.js
@@ -24,5 +24,6 @@ app.get('*', (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`server running on port ${PORT}`);
 });
